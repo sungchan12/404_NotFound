07824cd462967299586b5846aeab5af59e0e6dca
sungchean <chanyouunng1@gmail.com>
2025-06-17 18:36:13 +0900

    6/17

---
+++ python/beakjoon_1018.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+chess_color = []
+for _ in range(n):
+    chess_color.append(input())
+# print(chess_color)
+
+for i in range(n-7):
+    for j in range(m-7):
+        
