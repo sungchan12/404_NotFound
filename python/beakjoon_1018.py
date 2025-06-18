3ebc241b7f1fec570b389d8c9abd0117af84a200
sungchean <chanyouunng1@gmail.com>
2025-06-18 23:25:44 +0900

    6/18

--- python/beakjoon_1018.py
+++ python/beakjoon_1018.py
@@ -3,26 +3,25 @@ chess_color = []
 for _ in range(n):
     chess_color.append(input())
 # print(chess_color)
-
-for s in range(n-7):
-    for e in range(m-7):
-        #if chess_color[s][e] == 'W':
-            # print("white")
-        #else:
-            #print("black")
+cnt = []
+for column in range(n-7):
+    for row in range(m-7):
+        # print(column, row)
         white_cnt = 0
         black_cnt = 0
-        for k in range(s, s + 8):
-            for l in range(e, e + 8):
-                if (k+l)%2 == 0:
-                    if chess_color[k][l] != 'W':
+        for i in range(column, column + 8):
+            for j in range(row, row + 8):
+                if (i+j) % 2 == 0:
+                    if chess_color[i][j] != 'W':
                         white_cnt += 1
                     else:
                         black_cnt += 1
                 else:
-                    if chess_color[k][l] != 'W':
+                    if chess_color[i][j] != 'W':
                         black_cnt += 1
                     else:
                         white_cnt += 1
-                        
-print(min(white_cnt, black_cnt))
+        cnt.append(white_cnt)
+        cnt.append(black_cnt)
+# print(cnt)
+print(min(cnt))
