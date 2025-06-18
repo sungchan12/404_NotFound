31d90a70f54e9a7d4f85915d6824a0095f8be652
sungchean <chanyouunng1@gmail.com>
2025-06-19 01:25:46 +0900

    6/19

--- python/beakjoon_1051.py
+++ python/beakjoon_1051.py
@@ -1,6 +1,21 @@
 import sys
 input = sys.stdin.readline
+
+def find_max(value):
+    for i in range(n-value+1):
+        for j in range(m-value+1):
+            #0,2 2,0
+            if s[i][j] == s[i+value-1][j] == s[i][j+value-1] == s[i+value-1][j+value-1]:
+                return True
+    return False
+
 n, m = map(int, input().split())
 s = []
 for i in range(n):
     s.append(list(input().strip()))
+
+side = min(n, m)
+for k in range(side, 0, -1):
+    if find_max(k):
+        print(k * k)
+        break
