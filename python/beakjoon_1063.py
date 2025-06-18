1b103e71f2f42d80f3ffb2edab1f7ff36037ab08
sungchean <chanyouunng1@gmail.com>
2025-06-19 01:55:55 +0900

    6/19

--- python/beakjoon_1063.py
+++ python/beakjoon_1063.py
@@ -2,4 +2,13 @@ import sys
 
 input = sys.stdin.readline
 king_info, stone, loop = input().split()
-print(king_info, stone, loop)
+loop = int(loop)
+# print(king_info, stone, loop)
+chess_board = []
+for i in range(8):
+    row = []
+    for j in range(8):
+        row.append(0)
+    chess_board.append(row)
+
+def 
