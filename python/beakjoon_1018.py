9a5938e10ae003115ce75ffac77aeb4f6208b06f
sungchean <chanyouunng1@gmail.com>
2025-06-18 21:28:00 +0900

    6/18

--- python/beakjoon_1018.py
+++ python/beakjoon_1018.py
@@ -6,11 +6,23 @@ for _ in range(n):
 
 for s in range(n-7):
     for e in range(m-7):
-        if chess_color[s][e] == 'W':
-            print("white")
-        elif chess_color[s][e] == 'B':
-            print("black")
-        for k in range(s + 7):
-            for l in range(e + 7):
-                if (s+e)%2 == 0:
-                    
+        #if chess_color[s][e] == 'W':
+            # print("white")
+        #else:
+            #print("black")
+        white_cnt = 0
+        black_cnt = 0
+        for k in range(s, s + 8):
+            for l in range(e, e + 8):
+                if (k+l)%2 == 0:
+                    if chess_color[k][l] != 'W':
+                        white_cnt += 1
+                    else:
+                        black_cnt += 1
+                else:
+                    if chess_color[k][l] != 'W':
+                        black_cnt += 1
+                    else:
+                        white_cnt += 1
+                        
+print(min(white_cnt, black_cnt))
