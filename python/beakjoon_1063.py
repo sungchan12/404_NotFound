830aa60e9fb752b1c416c8299856380fbe6b4ea5
sungchean <chanyouunng1@gmail.com>
2025-06-19 13:51:21 +0900

    6/19

--- python/beakjoon_1063.py
+++ python/beakjoon_1063.py
@@ -2,6 +2,8 @@ import sys
 
 input = sys.stdin.readline
 king_info, stone, loop = input().split()
+king_info = king_info.lower()
+stone = king_info.lower()
 loop = int(loop)
 # print(king_info, stone, loop)
 chess_board = []
@@ -16,9 +18,25 @@ def array_to_chess(row, col):
     chess_col = chr(ord('a') + col)  
     chess_row = 8 - row             
     return chess_col + str(chess_row)
- 
+
+def get_king_index(king_info):
+    row = int(king_info[1])
+    column = king_info[0]
+    print(column, row)
+    return_col = ord(column) - ord('a')
+    return_row = 8 - row
+    return return_col, return_row
+
 for i in range(8):
     for j in range(8):
         if chess_board[i][j] == 0:
             chess_board[i][j] = array_to_chess(i, j)
-# print(chess_board)
+
+
+print(chess_board)
+print(get_king_index(king_info))
+
+# for _ in range(loop):
+#     order = input().strip()
+#     if order == 'B':
+        
