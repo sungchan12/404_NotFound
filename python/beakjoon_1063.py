e6f20f877f7710c5f94ac051ff8f66025f375ce3
sungchean <chanyouunng1@gmail.com>
2025-06-19 13:27:13 +0900

    6/19

--- python/beakjoon_1063.py
+++ python/beakjoon_1063.py
@@ -5,10 +5,20 @@ king_info, stone, loop = input().split()
 loop = int(loop)
 # print(king_info, stone, loop)
 chess_board = []
-for i in range(8):
+for _ in range(8):
     row = []
-    for j in range(8):
+    for _ in range(8):
         row.append(0)
     chess_board.append(row)
+# print(chess_board)
 
-def 
+def array_to_chess(row, col):
+    chess_col = chr(ord('a') + col)  
+    chess_row = 8 - row             
+    return chess_col + str(chess_row)
+ 
+for i in range(8):
+    for j in range(8):
+        if chess_board[i][j] == 0:
+            chess_board[i][j] = array_to_chess(i, j)
+# print(chess_board)
