879062f558d8c1a8c7f729787fc4f1439c775086
sungchean <chanyouunng1@gmail.com>
2025-06-20 13:45:31 +0900

    6/20

--- python/beakjoon_1063.py
+++ python/beakjoon_1063.py
@@ -1,9 +1,9 @@
 import sys
 
 input = sys.stdin.readline
-king_info, stone, loop = input().split()
+king_info, stone_info, loop = input().split()
 king_info = king_info.lower()
-stone = king_info.lower()
+stone = stone_info.lower()
 loop = int(loop)
 # print(king_info, stone, loop)
 chess_board = []
@@ -19,9 +19,9 @@ def array_to_chess(row, col):
     chess_row = 8 - row             
     return chess_col + str(chess_row)
 
-def get_king_index(king_info):
-    row = int(king_info[1])
-    column = king_info[0]
+def get_index(info):
+    row = int(info[1])
+    column = info[0]
     # print(column, row)
     return_col = ord(column) - ord('a')
     return_row = 8 - row
@@ -31,18 +31,20 @@ for i in range(8):
     for j in range(8):
         if chess_board[i][j] == 0:
             chess_board[i][j] = array_to_chess(i, j)
-
 # print(chess_board)
-# print(get_king_index(king_info))
-x, y = map(int, get_king_index(king_info))
+
+king_x, king_y = map(int, get_index(king_info))
+stone_x, stone_y = map(int, get_index(stone))
+print("king_x : ", king_x, " king_y : ", king_y)
+print("stone_x : ", stone_x, " stone_y : ", stone_y)
 
 for _ in range(loop):
     order = input().strip()
     # print(x, y)
     if order == 'B':
-        if x <= 0 or y >= 7:
+        if king_y >= 7:
             pass
     else:
-        y += 1
+        king_y += 1
         # print(x, y)
         # print(array_to_chess(y, x))
