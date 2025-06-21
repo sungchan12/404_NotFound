9e1b0af2329592a8a058eab45f88c02b01092b07
sungchean <chanyouunng1@gmail.com>
2025-06-21 19:34:23 +0900

    6/21

--- python/beakjoon_1205.py
+++ python/beakjoon_1205.py
@@ -3,4 +3,20 @@ input = sys.stdin.readline
 
 n, new_score, p = map(int, input().split())
 
-print()
+if (n == 0):
+    print(1)
+else:
+    score_list = list(map(int, input().split()))
+    score_list.sort(reverse=True)
+    result = 0
+    if score_list[-1] == new_score:
+        print(-1)
+    else: 
+        for i in range(len(score_list)):
+            if score_list[i] <= new_score:
+                result = i+1
+                print(result)
+                break
+            
+    # print(n, new_score, p)
+    # print(score_list)
