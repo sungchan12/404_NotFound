5c13ee277c0fea03020c94a37405171321928657
sungchean <chanyouunng1@gmail.com>
2025-06-23 00:56:25 +0900

    6/23

---
+++ python/beakjoon_1244.py
@@ -0,0 +1,9 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+status = list(map(int, input().split()))
+print("status : ", status)
+student_num = int(input())
+print("student_num : ", student_num)
+print(status)
