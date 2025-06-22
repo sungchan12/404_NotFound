6ee0c4545204e3952c3a24e284b8fa532f10aa11
sungchean <chanyouunng1@gmail.com>
2025-06-23 00:57:19 +0900

    6/23

--- python/beakjoon_1244.py
+++ python/beakjoon_1244.py
@@ -6,4 +6,14 @@ status = list(map(int, input().split()))
 print("status : ", status)
 student_num = int(input())
 print("student_num : ", student_num)
+for _ in range(student_num):
+    gender, switch = map(int, input().split())
+    print("gender : ", gender, " switch_num : ", switch)
+    if gender == 1:
+        for i in range(len(status)):
+            if (i+1) % switch == 0:
+                if status[i] == 0:
+                    status[i] = 1
+                else:
+                    status[i] = 0
 print(status)
