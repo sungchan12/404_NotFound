e296253b86ab501fb23efd87c54c5e6e3105c738
sungchean <chanyouunng1@gmail.com>
2025-06-22 20:26:41 +0900

    6/22

--- python/beakjoon_1235.py
+++ python/beakjoon_1235.py
@@ -1,1 +1,14 @@
 n = int(input())
+number = []
+cnt = 0
+for _ in range(n):
+    num = str(input())
+    number.append(int(num[4:7]))
+print(number)
+temp = []
+for i in range(len(number)):
+    if number[i] in temp:
+        pass
+    else:
+        temp.append(number[i])
+print(len(temp))
