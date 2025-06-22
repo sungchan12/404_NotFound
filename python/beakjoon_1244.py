b4de2df0d552d02e302591a904cbd9f09734bf38
sungchean <chanyouunng1@gmail.com>
2025-06-23 01:12:23 +0900

    6/23

--- python/beakjoon_1244.py
+++ python/beakjoon_1244.py
@@ -3,12 +3,12 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 status = list(map(int, input().split()))
-print("status : ", status)
+# print("status : ", status)
 student_num = int(input())
-print("student_num : ", student_num)
+# print("student_num : ", student_num)
 for _ in range(student_num):
     gender, switch = map(int, input().split())
-    print("gender : ", gender, " switch_num : ", switch)
+    # print("gender : ", gender, " switch_num : ", switch)
     if gender == 1:
         for i in range(len(status)):
             if (i+1) % switch == 0:
@@ -18,25 +18,22 @@ for _ in range(student_num):
                     status[i] = 0
     else:
         center = switch-1
-        left = switch-2
-        right = switch
-        if (status[left] == status[right]):
-            if status[center] == 0:
-                status[center] = 1
+        left = center-1
+        right = center+1
+        
+        status[center] = 1 - status[center]
+        
+        while (left >= 0 and right < n and status[left] == status[right]):
+            if status[left] == 0:
+                status[left] = 1
+                status[right] = 1
             else:
-                status[center] = 0
-            while (left >= 0 and right < n):
-                if status[left] == 0:
-                    status[left] = 1
-                    status[right] = 1
-                else:
-                    status[left] = 0
-                    status[right] = 0
-                left -= 1
-                right += 1
-        else:
-            if status[center] == 0:
-                status[center] = 1
-            else:
-                status[center] = 0
-print(status)
+                status[left] = 0
+                status[right] = 0
+            left -= 1
+            right += 1
+# print(status)
+for i in range(len(status)):
+    print(status[i], end=" ")
+    if (i + 1) % 20 == 0:
+        print()
