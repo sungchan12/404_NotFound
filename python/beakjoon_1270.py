2145ec87ea5b8388b6ad75d63c03c660f01ed53b
sungchean <chanyouunng1@gmail.com>
2025-06-23 18:32:09 +0900

    6/23

--- python/beakjoon_1270.py
+++ python/beakjoon_1270.py
@@ -7,9 +7,15 @@ for i in range(n):
     temp = land[i][1:]
     for num in temp:
         # print(num)
-        if num in dic[num]:
+        if num in dic:
             dic[num] += 1
         else:
             dic[num] = 1
-        print(dic)
-print(land)
+        # print(dic)
+    max_key = max(dic, key=dic.get)
+    # print(dic[max_key])
+    if dic[max_key] > land[i][0] / 2:
+        print(max_key)
+    else:
+        print("SYJKGW")
+# print(land)
