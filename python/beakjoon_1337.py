236644855430375751774723b19baef43118b508
sungchean <chanyouunng1@gmail.com>
2025-06-26 03:46:40 +0900

    6/26

--- python/beakjoon_1337.py
+++ python/beakjoon_1337.py
@@ -2,10 +2,18 @@ import sys
 input = sys.stdin.readline
 n = int(input().strip())
 num_list = []
-
+cnt = 0
 for _ in range(n):
     num_list.append(int(input().strip()))
+num_list.sort()
 print(num_list)
 
 if len(num_list) < 5:
     print(5-len(num_list))
+
+for i in range(len(num_list)):
+    print(i)
+    if num_list[i+1] - num_list[i] == 1:
+        print(num_list[i+1], num_list[i])
+        cnt += num_list[i+1] - num_list[i] -1
+print(cnt)
