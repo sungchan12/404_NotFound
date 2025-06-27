f8c478de86ebd7b0c55264eb6238256bb3f91b75
sungchean <chanyouunng1@gmail.com>
2025-06-28 00:01:52 +0900

    6/28

--- python/beakjoon_1340.py
+++ python/beakjoon_1340.py
@@ -7,3 +7,14 @@ day = int(day[:-1])
 year = int(year)
 minutes, second = map(int, time.split(":"))
 
+months = {
+    'January': 1, 'February': 2, 'March': 3, 'April': 4,
+    'May': 5, 'June': 6, 'July': 7, 'August': 8,
+    'September': 9, 'October': 10, 'November': 11, 'December': 12
+}
+
+days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def check_yoon(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        days[1] = 29
