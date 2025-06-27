f0022961914612c7a17978e6cbd1f798ad96ae4b
sungchean <chanyouunng1@gmail.com>
2025-06-28 00:11:29 +0900

    6/28

--- python/beakjoon_1340.py
+++ python/beakjoon_1340.py
@@ -14,7 +14,15 @@ months = {
 }
 
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+total = 365
 
 def check_yoon(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        days[1] = 29
+        return True
+    else:
+        return False
+
+if check_yoon(year):
+    days[1] = 29
+    total = 366
+
