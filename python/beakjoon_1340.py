3aa5d4687a9f9b9b6a0be57e6a64da2f66c41bd4
sungchean <chanyouunng1@gmail.com>
2025-06-28 01:44:08 +0900

    6/28

--- python/beakjoon_1340.py
+++ python/beakjoon_1340.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 month, day, year, time = input().split()
 day = int(day[:-1])
 year = int(year)
-minutes, second = map(int, time.split(":"))
+hours, minutes = map(int, time.split(":"))
 
 months = {
     'January': 1, 'February': 2, 'March': 3, 'April': 4,
@@ -26,3 +26,16 @@ if check_yoon(year):
     days[1] = 29
     total = 366
 
+# print(days[1], total)
+
+days_sum = day-1
+for i in range(months.get(month)-1):
+    days_sum += days[i]
+
+total_sec = total * 24 * 60 * 60
+total_day_sec = days_sum * 24 * 60 * 60
+current_time_sec = (minutes * 60)
+
+elapsed_sec = total_day_sec + current_time_sec
+progress = (elapsed_sec / total_sec) * 100
+print(progress)
