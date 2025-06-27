50c63700dfe0c78af56e51699f25540b7e1d7ccc
sungchean <chanyouunng1@gmail.com>
2025-06-27 23:25:23 +0900

    6/27

--- python/beakjoon_1337.py
+++ python/beakjoon_1337.py
@@ -2,4 +2,16 @@ import sys
 input = sys.stdin.readline
 n = int(input().strip())
 num_list = []
+cnt_list = []
+for _ in range(n):
+    num_list.append(int(input().strip()))
+# print(num_list)
 
+for i in range(0, len(num_list)):
+    # print(num_list[i])
+    cnt = 0
+    for j in range(num_list[i], num_list[i]+5):
+        if j not in num_list:
+            cnt += 1
+    cnt_list.append(cnt)
+print(min(cnt_list))
