6adb2ba8432e9a88f2550b381f143cdf00de7a53
sungchean <chanyouunng1@gmail.com>
2025-06-29 15:14:26 +0900

    6/29 어렵네

--- python/beakjoon_1730.py
+++ python/beakjoon_1730.py
@@ -12,3 +12,15 @@ for i in range(n):
         row.append(".")
     root.append(row)
 # print(root)
+
+direction = {"U" : (-1, 0), "R" : (0, 1), "D" : (1, 0), "L" : (0, -1)}
+
+sr, sc = 0, 0
+
+for w in way:
+    # print(w)
+    if w == "D":
+        wr, wc = direction.get(w)
+        # print(wr, wc)
+        sr += wr
+        sc += wc
