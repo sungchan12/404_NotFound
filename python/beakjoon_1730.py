71092586ea19070a7c0f0fc42051a24678cb13c9
sungchean <chanyouunng1@gmail.com>
2025-06-29 19:31:51 +0900

    6/29

--- python/beakjoon_1730.py
+++ python/beakjoon_1730.py
@@ -4,14 +4,12 @@ input = sys.stdin.readline
 
 n = int(input())
 way = input().strip()
-# print(len(way))
 root = []
 for i in range(n):
-    row = []
-    for j in range(n):
-        row.append(".")
-    root.append(row)
-# print(root)
+   row = []
+   for j in range(n):
+       row.append(".")
+   root.append(row)
 
 direction = {"U" : (-1, 0), "R" : (0, 1), "D" : (1, 0), "L" : (0, -1)}
 
@@ -19,23 +17,9 @@ sr, sc = 0, 0
 
 for w in way:
     wr, wc = direction.get(w)
-    # print(wr, wc)
-    if w in "UD":
-        if root[sr][sc] == ".":
-            root[sr][sc] = "|"
-        elif root[sr][sc] == "-":
-            root[sr][sc] = "+"
-    else:
-        if root[sr][sc] == ".":
-            root[sr][sc] = "-"
-        elif root[sr][sc] == "|":
-            root[sr][sc] = "+"
-    sr += wr
-    sc += wc
-    if (sr < 0 or sr >= n) or (sc < 0 or sc >= n):
-        sr = sr - wr
-        sc = sc - wc
-    else:
+    move_sr, move_sc = (sr+wr), (sc+wc)
+    # print(move_sr, move_sc)
+    if (0 <= move_sr < n) and (0 <= move_sc < n):
         if w in "UD":
             if root[sr][sc] == ".":
                 root[sr][sc] = "|"
@@ -46,7 +30,17 @@ for w in way:
                 root[sr][sc] = "-"
             elif root[sr][sc] == "|":
                 root[sr][sc] = "+"
-
-# print(sr, sc)
+        sr, sc = move_sr, move_sc
+        # print(sr, sc)
+        if w in "UD":
+            if root[sr][sc] == ".":
+                root[sr][sc] = "|"
+            elif root[sr][sc] == "-":
+                root[sr][sc] = "+"
+        else:
+            if root[sr][sc] == ".":
+                root[sr][sc] = "-"
+            elif root[sr][sc] == "|":
+                root[sr][sc] = "+"
 for row in root:
    print(''.join(row))
