4ccbb1ddce9993a29ed04ab3873b10753e3e8bb7
sungchean <chanyouunng1@gmail.com>
2025-06-29 17:39:40 +0900

    6/29

--- python/beakjoon_1730.py
+++ python/beakjoon_1730.py
@@ -18,9 +18,35 @@ direction = {"U" : (-1, 0), "R" : (0, 1), "D" : (1, 0), "L" : (0, -1)}
 sr, sc = 0, 0
 
 for w in way:
-    # print(w)
-    if w == "D":
-        wr, wc = direction.get(w)
-        # print(wr, wc)
-        sr += wr
-        sc += wc
+    wr, wc = direction.get(w)
+    # print(wr, wc)
+    if w in "UD":
+        if root[sr][sc] == ".":
+            root[sr][sc] = "|"
+        elif root[sr][sc] == "-":
+            root[sr][sc] = "+"
+    else:
+        if root[sr][sc] == ".":
+            root[sr][sc] = "-"
+        elif root[sr][sc] == "|":
+            root[sr][sc] = "+"
+    sr += wr
+    sc += wc
+    if (sr < 0 or sr >= n) or (sc < 0 or sc >= n):
+        sr = sr - wr
+        sc = sc - wc
+    else:
+        if w in "UD":
+            if root[sr][sc] == ".":
+                root[sr][sc] = "|"
+            elif root[sr][sc] == "-":
+                root[sr][sc] = "+"
+        else:
+            if root[sr][sc] == ".":
+                root[sr][sc] = "-"
+            elif root[sr][sc] == "|":
+                root[sr][sc] = "+"
+
+# print(sr, sc)
+for row in root:
+   print(''.join(row))
