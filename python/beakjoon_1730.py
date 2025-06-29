b08ac6b973c02d0066864cace76d834463de86e4
sungchean <chanyouunng1@gmail.com>
2025-06-29 19:34:10 +0900

    6/29

--- python/beakjoon_1730.py
+++ python/beakjoon_1730.py
@@ -4,13 +4,14 @@ input = sys.stdin.readline
 
 n = int(input())
 way = input().strip()
+# print(len(way))
 root = []
 for i in range(n):
    row = []
    for j in range(n):
        row.append(".")
    root.append(row)
-
+# print(root)
 direction = {"U" : (-1, 0), "R" : (0, 1), "D" : (1, 0), "L" : (0, -1)}
 
 sr, sc = 0, 0
