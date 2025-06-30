5a04cea471343e4fcd8632a349f54872c8ee61e2
sungchean <chanyouunng1@gmail.com>
2025-06-30 23:48:43 +0900

    6/30

--- python/beakjoon_1614.py
+++ python/beakjoon_1614.py
@@ -1,5 +1,7 @@
-hurt_fin = int(input())
-use_cnt = int(input())
+import sys
+
+hurt_fin = int(sys.stdin.readline()) # 다친 손가락
+use_cnt = int(sys.stdin.readline()) # 사용 횟수
 # print(hurt_fin, use_cnt)
 fin_cnt = 0
 num = 0
@@ -8,9 +10,10 @@ direction = 1
 while fin_cnt <= use_cnt:
     num += 1
     finger += direction
+    if finger == hurt_fin:
+        fin_cnt += 1    
     if finger == 5:
         direction = -1
     elif finger == 1:
         direction = 1
-    if finger == hurt_fin:
-        fin_cnt += 1
+print(num-1)
