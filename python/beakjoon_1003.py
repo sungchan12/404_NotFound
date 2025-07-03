edac45433a10b7021593659539dd28bc03d46490
sungchean <chanyouunng1@gmail.com>
2025-07-03 20:05:34 +0900

    7/3

--- python/beakjoon_1003.py
+++ python/beakjoon_1003.py
@@ -1,25 +1,16 @@
-import sys
+t = int(input())
+zero_cnt = [0] * 41
+one_cnt = [0] * 41
+# print(zero_cnt)
+# print(one_cnt)
 
-input = sys.stdin.readline
-def fibonacci(num):
-    global zero_cnt, one_cnt
-    if num == 0:
-        zero_cnt += 1
-        return 0
-    elif num == 1:
-        one_cnt += 1
-        return 1
-    else:
-        return fibonacci(num - 1) + fibonacci(num - 2)
-
-n = int(input())
-
-for _ in range(n):
+for _ in range(t):
     num = int(input())
+    zero_cnt[0], one_cnt[0] = 1, 0
+    zero_cnt[1], one_cnt[1] = 0, 1
 
-    zero_cnt = 0
-    one_cnt = 0
+    for i in range(2, num+1):
+        zero_cnt[i] = zero_cnt[i-1] + zero_cnt[i-2]
+        one_cnt[i] = one_cnt[i-1] + one_cnt[i-2]
 
-    fibonacci(num)
-    
-    print(zero_cnt, one_cnt)
+    print(zero_cnt[num], one_cnt[num])
