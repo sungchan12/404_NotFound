9cdc38038bbc7bff05cd00c61ce4d542c1c3fd72
sungchean <chanyouunng1@gmail.com>
2025-07-06 18:38:43 +0900

    7/6

--- python/beakjoon_1748.py
+++ python/beakjoon_1748.py
@@ -3,7 +3,13 @@ import sys
 n_str = sys.stdin.readline().strip()
 n_int = int(n_str)
 length = len(n_str)
-print(n_int, length)
+
 ans = 0
+
+for i in range(1, length):
+    num_count = 9 * (10**(i-1))
+    ans += num_count * i
+
+ans += (n_int - (10**(length-1)) + 1) * length
 
 print(ans)
