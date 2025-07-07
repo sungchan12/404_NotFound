f629bcb86e63f215a3ebbff8a4642f1b947ad10f
sungchean <chanyouunng1@gmail.com>
2025-07-07 23:22:46 +0900

    7/7

---
+++ python/beakjoon_1032.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+sentence = list(input())
+
+for i in range(n-1):
+    other_sen = input()
+    for j in range(len(sentence)):
+        if sentence[j] != other_sen[j]:
+            sentence[j] = "?"
+
+for i in sentence:
+    print(i, end="")
