72078fbc63dda1161925557b7dc57d1361d99885
sungchean <chanyouunng1@gmail.com>
2025-07-09 23:11:59 +0900

    7/9

--- python/beakjoon_1384.py
+++ python/beakjoon_1384.py
@@ -1,12 +1,31 @@
 group_cnt = 1
-n = int(input())
-student_list = []
-nasty_reports = []
+while True:
+    n = int(input())
+    if n == 0:
+        break
 
-for i in range(n):
-    parts = input().split()
-    student_list.append(parts[0])
-    nasty_reports.append(parts[1:])
-# print(student_list)
-# print(nasty_reports)
+    student_list = []
+    nasty_reports = []
 
+    for i in range(n):
+        parts = input().split()
+        student_list.append(parts[0])
+        nasty_reports.append(parts[1:])
+    # print(student_list)
+    # print(nasty_reports)
+    cnt = 0
+    print("Group %d" % group_cnt)
+    for i in range(n):
+        for j in range(len(nasty_reports[i])):
+            if nasty_reports[i][j] == "N":
+                # print(i, j)
+                if i > j:
+                    name = student_list[i - (j+1)]
+                else:
+                    name = student_list[len(student_list) - (j+1)]
+                print("%s was nasty to %s" % (name, student_list[i]))
+                cnt += 1
+    group_cnt += 1
+    if cnt == 0:
+        print("Nobody was nasty")
+    print()
