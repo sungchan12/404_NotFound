36bca14b601ccc088fa079ce6c57374e0c8e62a8
sungchean <chanyouunng1@gmail.com>
2025-07-09 22:00:31 +0900

    7/9

--- python/beakjoon_1380.py
+++ python/beakjoon_1380.py
@@ -1,5 +1,25 @@
-steal_neckgirl_cnt = int(input())
-girl_list = []
-for _ in range(steal_neckgirl_cnt):
-    girl_list.append(input())
+scenario_num = 1
+while True:
+    steal_neckgirl_cnt = int(input())
 
+    if steal_neckgirl_cnt == 0:
+        break
+    else:
+        students = []
+        for _ in range(steal_neckgirl_cnt):
+            students.append(input())
+        # print(girl_list)
+
+        sinario_list = []
+        for i in range(2*steal_neckgirl_cnt-1):
+            st_num, sinario = input().split()
+            sinario_list.append(int(st_num))
+        # print(sinario_list)
+
+        for student_num in sinario_list:
+            if sinario_list.count(student_num) == 1:
+                # print(student_num)
+                index = student_num-1
+                break
+        print(scenario_num, students[index])
+        scenario_num += 1
